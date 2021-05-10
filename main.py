cd6055653eaa6f85705bd2722c46f071f1de961a
Mols1993 <mols1993@gmail.com>
2021-05-10 03:09:39 -0400

    Added monster images
    Fixed some challenge conflicts

--- main.py
+++ main.py
@@ -9,49 +9,49 @@ root.geometry("+50+50")
 root.title("Rise to the Challenge")
 
 monsterList = [
-    "Aknosom", 
-	"Almudron", 
-	"Anjanath", 
-	"Arzuros", 
-	"Apex Arzuros", 
-	"Barioth", 
-	"Barroth", 
-	"Bazelgeuse", 
-	"Basarios", 
-	"Bishaten", 
-	"Chameleos", 
-	"Diablos", 
-	"Apex Diablos", 
-	"Goss Harag", 
-	"Great Baggi", 
-	"Great Izuchi", 
-	"Great Wroggi", 
-	"Jyuratodus", 
-	"Khezu", 
-	"Kulu-Ya-Ku", 
-	"Kushala Daora", 
-	"Lagombi", 
-	"Magnamalo", 
-	"Mizutsune", 
-	"Apex Mizutsune", 
-	"Nargacuga", 
-	"Pukei-Pukei", 
-	"Rajang", 
-	"Rakna-Kadaki", 
-	"Rathalos", 
-	"Apex Rathalos", 
-	"Rathian", 
-	"Apex Rathian", 
-	"Royal Ludroth", 
-	"Somnacanth", 
-	"Teostra", 
-	"Tetranadon", 
-	"Thunder Serpent Narwa", 
-	"Tigrex", 
-	"Tobi-Kadachi", 
-	"Wind Serpent Ibushi", 
-	"Volvidon", 
-	"Zinogre"
+    ["Aknosom", PIL.ImageTk.PhotoImage(PIL.Image.open("images/aknosom.png"))], 
+	["Almudron", PIL.ImageTk.PhotoImage(PIL.Image.open("images/almudron.png"))], 
+	["Anjanath", PIL.ImageTk.PhotoImage(PIL.Image.open("images/anjanath.png"))], 
+	["Arzuros", PIL.ImageTk.PhotoImage(PIL.Image.open("images/arzuros.png"))], 
+	["Apex Arzuros", PIL.ImageTk.PhotoImage(PIL.Image.open("images/apexArzuros.png"))], 
+	["Barioth", PIL.ImageTk.PhotoImage(PIL.Image.open("images/barioth.png"))], 
+	["Barroth", PIL.ImageTk.PhotoImage(PIL.Image.open("images/barroth.png"))], 
+	["Bazelgeuse", PIL.ImageTk.PhotoImage(PIL.Image.open("images/bazelgeuse.png"))], 
+	["Basarios", PIL.ImageTk.PhotoImage(PIL.Image.open("images/basarios.png"))], 
+	["Bishaten", PIL.ImageTk.PhotoImage(PIL.Image.open("images/bishaten.png"))], 
+	["Chameleos", PIL.ImageTk.PhotoImage(PIL.Image.open("images/chameleos.png"))], 
+	["Diablos", PIL.ImageTk.PhotoImage(PIL.Image.open("images/diablos.png"))], 
+	["Apex Diablos", PIL.ImageTk.PhotoImage(PIL.Image.open("images/apexDiablos.png"))], 
+	["Goss Harag", PIL.ImageTk.PhotoImage(PIL.Image.open("images/gossHarag.png"))], 
+	["Great Baggi", PIL.ImageTk.PhotoImage(PIL.Image.open("images/greatBaggi.png"))], 
+	["Great Izuchi", PIL.ImageTk.PhotoImage(PIL.Image.open("images/greatIzuchi.png"))], 
+	["Great Wroggi", PIL.ImageTk.PhotoImage(PIL.Image.open("images/greatWroggi.png"))], 
+	["Jyuratodus", PIL.ImageTk.PhotoImage(PIL.Image.open("images/jyuratodus.png"))], 
+	["Khezu", PIL.ImageTk.PhotoImage(PIL.Image.open("images/khezu.png"))], 
+	["Kulu-Ya-Ku", PIL.ImageTk.PhotoImage(PIL.Image.open("images/kuluYaKu.png"))], 
+	["Kushala Daora", PIL.ImageTk.PhotoImage(PIL.Image.open("images/kushalaDaora.png"))], 
+	["Lagombi", PIL.ImageTk.PhotoImage(PIL.Image.open("images/lagombi.png"))], 
+	["Magnamalo", PIL.ImageTk.PhotoImage(PIL.Image.open("images/magnamalo.png"))], 
+	["Mizutsune", PIL.ImageTk.PhotoImage(PIL.Image.open("images/mizutsune.png"))], 
+	["Apex Mizutsune", PIL.ImageTk.PhotoImage(PIL.Image.open("images/apexMizutsune.png"))], 
+	["Nargacuga", PIL.ImageTk.PhotoImage(PIL.Image.open("images/nargacuga.png"))], 
+	["Pukei-Pukei", PIL.ImageTk.PhotoImage(PIL.Image.open("images/pukeiPukei.png"))], 
+	["Rajang", PIL.ImageTk.PhotoImage(PIL.Image.open("images/rajang.png"))], 
+	["Rakna-Kadaki", PIL.ImageTk.PhotoImage(PIL.Image.open("images/raknaKadaki.png"))], 
+	["Rathalos", PIL.ImageTk.PhotoImage(PIL.Image.open("images/rathalos.png"))], 
+	["Apex Rathalos", PIL.ImageTk.PhotoImage(PIL.Image.open("images/apexRathalos.png"))], 
+	["Rathian", PIL.ImageTk.PhotoImage(PIL.Image.open("images/rathian.png"))], 
+	["Apex Rathian", PIL.ImageTk.PhotoImage(PIL.Image.open("images/apexRathian.png"))], 
+	["Royal Ludroth", PIL.ImageTk.PhotoImage(PIL.Image.open("images/royalLudroth.png"))], 
+	["Somnacanth", PIL.ImageTk.PhotoImage(PIL.Image.open("images/somnacanth.png"))], 
+	["Teostra", PIL.ImageTk.PhotoImage(PIL.Image.open("images/teostra.png"))], 
+	["Tetranadon", PIL.ImageTk.PhotoImage(PIL.Image.open("images/tetranadon.png"))], 
+	["Thunder Serpent Narwa", PIL.ImageTk.PhotoImage(PIL.Image.open("images/narwa.png"))], 
+	["Tigrex", PIL.ImageTk.PhotoImage(PIL.Image.open("images/tigrex.png"))], 
+	["Tobi-Kadachi", PIL.ImageTk.PhotoImage(PIL.Image.open("images/tobiKadachi.png"))], 
+	["Wind Serpent Ibushi", PIL.ImageTk.PhotoImage(PIL.Image.open("images/ibushi.png"))], 
+	["Volvidon", PIL.ImageTk.PhotoImage(PIL.Image.open("images/volvidon.png"))], 
+	["Zinogre", PIL.ImageTk.PhotoImage(PIL.Image.open("images/zinogre.png"))]
 ]
 
 #[Weapon name, ID to filter compatible challenges, image icon]
@@ -82,12 +82,12 @@ challengeList = [
     ["Hardcore (No carting)", 5, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], []],
     ["Low rank gear", 6, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], []],
     ["No dodging", 7, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], []],
-    ["No sharpening", 8, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10]],
+    ["No sharpening", 8, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 34]],
     ["Only Normal 1/No Coating", 9, [11, 12, 13], [10, 33, 34]],
     ["Only attack from palamute", 10, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [12, 18, 22, 24, 25, 26, 27, 29, 32, 33]],
     ["Inverted controls (or normal, the ones you don't use)", 11, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], []],
     ["Silkbind attacks only", 12, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [10, 13, 14]],
-    ["No silkbind attacks", 13, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [10, 12, 14]],
+    ["No silkbind attacks", 13, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [10, 12, 14, 34]],
     ["No wirebug (Play like it's MH1)", 14, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [0, 12, 13]],
     ["No moving while using items (Old school flex)", 15, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [4, 10]],
     ["Gyroscope camera", 16, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], []],
@@ -104,12 +104,12 @@ challengeList = [
     ["Axe mode only", 26, [8], [10, 27, 34]],
     ["Sword mode only", 27, [8, 9], [10, 26, 34]],
     ["No shield charging", 28, [9], [10, 34]],
-    ["Kinsect only", 29, [10], [10, 30, 34]],
+    ["Kinsect only", 29, [10], [10, 30, 32, 34]],
     ["No kinsect", 30, [10], [10, 29, 34]],
     ["No aerial attacks", 31, [10], [10, 32, 34]],
-    ["Aerial attacks only", 32, [10], [10, 31, 34]],
+    ["Aerial attacks only", 32, [10], [10, 29, 31, 34]],
     ["Melee only", 33, [11, 12, 13], [9, 10, 34]],
-    ["Items Only", 34, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [3, 4, 9, 10, 12, 13, 17, 18, 19, 20, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]]
+    ["Items Only", 34, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [3, 4, 8, 9, 10, 12, 13, 17, 18, 19, 20, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]]
 ]
 
 mainFrame = tk.Frame(root, borderwidth = 5, relief = tk.GROOVE)
@@ -134,7 +134,7 @@ for i in monsterList:
         xcor = xcor + 1
 
     var = tk.IntVar()
-    monsterCheckboxes.append([tk.Checkbutton(monsterSubFrames[subFrameCounter], text = i, width = 25, borderwidth = 5, relief = tk.RAISED, variable = var), var])
+    monsterCheckboxes.append([tk.Checkbutton(monsterSubFrames[subFrameCounter], text = i[0], width = 25, height = 1, borderwidth = 5, relief = tk.RAISED, variable = var), var])
     monsterCheckboxes[j][0].select()
     monsterCheckboxes[j][0].grid(row = ycor, column = xcor, sticky = "n", padx = 5, pady = 3)
     #print(monsterCheckboxes[j])
@@ -243,7 +243,17 @@ def generateChallenge():
 
     monster = monsterSelection[random.randint(0, len(monsterSelection) - 1)]
 
+    monsterImg = ""
+    for i in monsterList:
+        if(monster == i[0]):
+            monsterImg = i[1]
+
     weapon = weaponSelection[random.randint(0, len(weaponSelection) - 1)]
+
+    weaponImg = ""
+    for i in weaponList:
+        if(weapon == i[0]):
+            weaponImg = i[2]
 
     #Get weapon ID, to filter compatible challenges
     weaponID = 0
@@ -273,7 +283,7 @@ def generateChallenge():
         if(len(compatibleChallenges) == 0):
             break
 
-    newWindow(monster, weapon, challenges)
+    newWindow(monster, monsterImg, weapon, weaponImg, challenges)
 
 
 #Setup settings selection frame
@@ -291,16 +301,20 @@ challengeFrame.grid(row = 0, column = 2, sticky = "ns")
 settingsFrame.grid(row = 1, column = 0, columnspan = 3, sticky = "ew")
 
 
-def newWindow(monster, weapon, challenges):
-    root = tk.Tk()
+def newWindow(monster, monsterImg, weapon, weaponImg, challenges):
+    root = tk.Toplevel()
     root.minsize(300, 200)
-    root.title("Challenges")
+    root.title("Challenge")
     frame = tk.Frame(root, borderwidth = 5, relief = tk.GROOVE)
     monsterLabel = tk.Label(frame, text = "Monster:", font=("arial", 20))
     monsterSelected = tk.Label(frame, text = monster)
 
+    monsterImage = tk.Label(frame, image = monsterImg)
+
     weaponLabel = tk.Label(frame, text = "Weapon:", font=("arial", 20))
     weaponSelected = tk.Label(frame, text = weapon)
+
+    weaponImage = tk.Label(frame, image = weaponImg)
 
     challengesText = "\n".join(challenges)
     challengeLabel = tk.Label(frame, text = "Challenges:", font=("arial", 20))
@@ -308,8 +322,10 @@ def newWindow(monster, weapon, challenges):
 
     frame.pack()
     monsterLabel.pack()
+    monsterImage.pack()
     monsterSelected.pack()
     weaponLabel.pack()
+    weaponImage.pack()
     weaponSelected.pack()
     challengeLabel.pack()
     challengeSelected.pack()
